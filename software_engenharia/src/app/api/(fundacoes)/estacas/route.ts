2e22d491a9daa6d79ae0a3a07b9f745f82104da4
Diego Armando <127689958+DiegoASC287@users.noreply.github.com>
2025-12-16 16:58:47 -0300

    Update route.ts

--- software_engenharia/src/app/api/(fundacoes)/estacas/route.ts
+++ software_engenharia/src/app/api/(fundacoes)/estacas/route.ts
@@ -3,17 +3,18 @@ import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 const PropsEntradaEstaca = z.object({
     spt: z.array(z.object({
-        nspt: z.number().min(0),
+        nspt: z.coerce.number().min(0),
         tipo_solo: z.enum(TipoDeSolo),
         submerso: z.boolean(),
     })),
-    Nsd: z.number().min(0),
-    prof_apoio: z.number().min(0),
-    diametro: z.number().min(0),
+    Nsd: z.coerce.number().min(0),
+    prof_apoio: z.coerce.number().min(0),
+    diametro: z.coerce.number().min(0),
     espacamento: z.coerce.number(),
     tipo_estaca: z.enum(TipoEstaca),
-    fl: z.number().min(1),
-    fp: z.number().min(1)
+    fl: z.coerce.number().min(1),
+    fp: z.coerce.number().min(1),
+    cota_topo_estaca: z.coerce.number()
 })
 
 interface ResultadoCalculoEstaca {
@@ -38,13 +39,14 @@ interface ResultadoCalculoEstaca {
 type IPropsEntradaEstaca = z.infer<typeof PropsEntradaEstaca>;
 
 function calcCapAokiVelloso({ Nsd, diametro, tipo_estaca,
-    spt, prof_apoio, fl, fp, espacamento }: IPropsEntradaEstaca): ResultadoCalculoEstaca[] {
+    spt, prof_apoio, fl, fp, espacamento, cota_topo_estaca}: IPropsEntradaEstaca): ResultadoCalculoEstaca[] {
     const fator_reducao_grupo = BuscarFatorGrupo(espacamento, diametro, tipo_estaca)
     const { F1, F2 } = fatoresCorrecaoAoki(tipo_estaca, diametro);
     // Cálculo da capacidade de carga da estaca segundo Aoki & Velloso (1975)
     const resistecia: ResultadoCalculoEstaca[] = []
     let profundidade_atual = 1;
     while (profundidade_atual <= prof_apoio) {
+        const cota_atual = cota_topo_estaca-profundidade_atual
         const nspt_cur = spt[profundidade_atual - 1];
         const { kh, kv } = RetornarValorKhEKv({ diametro_largura: diametro, ...nspt_cur, profundidade: profundidade_atual })
         const { K, alfa } = ParamsTipoSoloAoki(nspt_cur?.tipo_solo ?? TipoDeSolo.ARGILA);
@@ -53,23 +55,24 @@ function calcCapAokiVelloso({ Nsd, diametro, tipo_estaca,
         let tensao_ponta_try = K * (nspt_cur?.nspt ?? 0) > 10000 ? 10000 : K * (nspt_cur?.nspt ?? 0)
         let Qp_cur_try = tensao_ponta_try * Math.PI * (diametro ** 2) / (4 * F1);
         let Qp_cur = 0;
+        const Nsd_pp_cur = Math.PI * (diametro ** 2) / 4 * (profundidade_atual) * 25 * 1.4
+        const Nsd_tot = Nsd + Nsd_pp_cur;
         const q_acum = resistecia.reduce((acc, cur) => acc + cur.Qs, 0)
-        if (profundidade_atual === prof_apoio || ((q_acum + qs_cur) / fl + Qp_cur_try / fp >= Nsd)) {
+        if (profundidade_atual === prof_apoio || ((q_acum + qs_cur) / fl + Qp_cur_try / fp >= Nsd_tot)) {
             Qp_cur = Qp_cur_try
         }
 
-        const qtot = q_acum + qs_cur + Qp_cur / fp;
+        const qtot = q_acum + qs_cur + Qp_cur;
         const qrd = (q_acum + qs_cur) / fl + Qp_cur / fp;
-        const Nsd_pp_cur = Math.PI * (diametro ** 2) / 4 * (profundidade_atual) * 25 * 1.4
-        const Nsd_tot = Nsd + Nsd_pp_cur;
 
         const teste = {
+            cota_atual,
             profundidade: profundidade_atual, tipo_solo: nspt_cur.tipo_solo, nspt: nspt_cur.nspt, F1, F2, K,
             Qs: qs_cur, Qp: Qp_cur, Qacum: qtot, Qrd: qrd, Nsd, Nsd_pp: Nsd_pp_cur, Nsd_tot, kh, kv, eta_g_grupo: fator_reducao_grupo
         }
         resistecia.push({...teste});
         console.log(teste)
-        if (resistecia.slice(-1)[0].Qrd >= Nsd_tot) {
+        if (resistecia.slice(-1)[0].Qrd > Nsd_tot) {
             return resistecia;
         }
         profundidade_atual += 1;
@@ -85,9 +88,9 @@ export async function POST(request: NextRequest) {
             return NextResponse.json({ error: resultado.error.issues.map(e => ({ message: e.message, path: e.path.join('.') })) }, { status: 400 });
         }
 
-        const { spt, Nsd, diametro, tipo_estaca, prof_apoio, fl, fp, espacamento } = resultado.data;
+        const { spt, Nsd, diametro, tipo_estaca, prof_apoio, fl, fp, espacamento, cota_topo_estaca } = resultado.data;
 
-        const resultados = calcCapAokiVelloso({ spt, Nsd, diametro, tipo_estaca, prof_apoio, fl, fp, espacamento });
+        const resultados = calcCapAokiVelloso({ spt, Nsd, diametro, tipo_estaca, prof_apoio, fl, fp, espacamento, cota_topo_estaca});
 
         return NextResponse.json({ resultados });
 
