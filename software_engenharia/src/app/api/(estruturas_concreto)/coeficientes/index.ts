f40648510e3093047af07bbcb285431612c293f4
Diego Armando <127689958+DiegoASC287@users.noreply.github.com>
2026-01-20 17:44:22 -0300

    ajustes

--- software_engenharia/src/app/api/(estruturas_concreto)/coeficientes/index.ts
+++ software_engenharia/src/app/api/(estruturas_concreto)/coeficientes/index.ts
@@ -3,7 +3,19 @@ import z from "zod"
 export const classe_concreto = ["C20", "C25", "C30", "C35", "C40",
     "C45", "C50", "C60", "C70", "C80", "C90"] as const;
 export type ClasseConcreto = typeof classe_concreto[number];
-
+export const sel_alfa_i: Record<ClasseConcreto, number> = {
+    "C20": 0.85,
+    "C25": 0.86,
+    "C30": 0.88,
+    "C35": 0.89,
+    "C40": 0.90,
+    "C45": 0.91,
+    "C50": 0.93,
+    "C60": 0.95,
+    "C70": 0.98,
+    "C80": 1.0,
+    "C90": 1.0
+};
 export const sel_fck_concreto: Record<ClasseConcreto, number> = {
     "C20": 20,
     "C25": 25,
@@ -18,19 +30,162 @@ export const sel_fck_concreto: Record<ClasseConcreto, number> = {
     "C90": 90
 };
 
+export function props_conc(classe_concreto: ClasseConcreto) {
+    const alfa_i = sel_alfa_i[classe_concreto]
+    const fck = sel_fck_concreto[classe_concreto]
+    return { fck, Ec: alfa_i * 5600 * Math.sqrt(fck) }
+}
+
+export const classe_aco = ["CA50", "CA60"] as const;
+export type ClasseAco = typeof classe_aco[number];
+
+export const sel_props_aco: Record<ClasseAco, { fyk: number, Es: number }> = {
+    "CA50": { Es: 200000, fyk: 500 },
+    "CA60": { Es: 200000, fyk: 600 }
+};
+
+export function props_steel(classe_aco: ClasseAco) {
+
+    return sel_props_aco[classe_aco]
+}
 
 export const diametros_comerciais = ["5", "6.3", "8", "10", "12.5", "16", "20", "25", "32"] as const
 export type DiametrosComerciais = typeof diametros_comerciais[number]
 
-export const sel_as: Record<DiametrosComerciais, {as: number}> = {
-    "5": {as: 0.196},
-    "6.3": {as: 0.31},
-    "8": {as: 0.5},
-    "10": {as: 0.785},
-    "12.5": {as: 1.22},
-    "16": {as: 2.01},
-    "20": {as: 3.14},
-    "25": {as: 4.91},
-    "32": {as: 8.04}
+export const sel_as: Record<DiametrosComerciais, { as: number }> = {
+    "5": { as: 0.196 },
+    "6.3": { as: 0.31 },
+    "8": { as: 0.5 },
+    "10": { as: 0.785 },
+    "12.5": { as: 1.22 },
+    "16": { as: 2.01 },
+    "20": { as: 3.14 },
+    "25": { as: 4.91 },
+    "32": { as: 8.04 }
 }
 
+export interface PropsLNII {
+    bw: number
+    alfa_e: number
+    as: number
+    as_linha: number
+    d: number
+    d_linha: number
+}
+
+export function calc_props_secao_ii({ bw, alfa_e, as, d }: PropsLNII) {
+    const xii = (-alfa_e * as + Math.sqrt((alfa_e * as) ** 2 + 2 * bw * alfa_e * as * d)) / bw
+    const Iii = bw * xii ** 3 / 3 + alfa_e * as * (d - xii) ** 2
+
+    return { xii, Iii }
+}
+
+
+interface CalcSigmaSProps {
+    alfa_e: number;
+    msd: number;
+    d: number;
+    xii: number;
+    Iii: number;
+}
+/**
+ * 
+ * @param xii Linha neutra em cm
+ * @param alfa_e Coeficiente adimensional
+ * @param d altura útil em cm
+ * @param msd momento fletor de cálculo (kNm)
+ * @param Iii momento de inércia para o estádio 2 em cm^4
+ * @returns tensão máxima em kN/cm²
+ */
+export function calc_sigma_s({ Iii, alfa_e, d, msd, xii }: CalcSigmaSProps) {
+    return alfa_e * msd * 100 * (d - xii) / Iii
+}
+
+interface PropsSigmaC {
+    msd: number
+    xii: number
+    Iii: number
+}
+
+/**
+ * 
+ * @param msd_min KNm
+ * @param Iii cm^4
+ * @param xii cm
+ * 
+ * @returns tensão em kN/cm²
+ */
+export function calc_sigma_c({msd, Iii, xii}:PropsSigmaC){
+    const sigma_c_min = xii > 30 ? msd*100*(xii-30)/Iii : 0
+    const sigma_c_max = msd*100*xii/Iii
+    const ni_c = 1/(1.5-0.5*sigma_c_min/sigma_c_max)
+    return {sigma_c_min, sigma_c_max, ni_c}
+}
+
+export function sel_diam_pino_D(diametro_arm: DiametrosComerciais, tipo_aco: ClasseAco) {
+    if (Number(diametro_arm) < 20) {
+        if (tipo_aco === "CA50") {
+            return 5 * Number(diametro_arm)
+        } else if (tipo_aco === "CA60") {
+            return 6 * Number(diametro_arm)
+        }
+        return null
+    } else {
+        if (tipo_aco === "CA50") {
+            return 8 * Number(diametro_arm)
+        }
+        return null
+    }
+}
+/**
+ * 
+ * @param diametro_barra diametro em mm
+ * @param tipo_aco Tipo do aço
+ * @returns Variação de tensão máxima em MPa
+ */
+export function sel_delta_fsd(diametro_barra: DiametrosComerciais, tipo_aco: ClasseAco) {
+    const D = sel_diam_pino_D(diametro_barra, tipo_aco)
+    if (D !== null) {
+        console.log("Infos entrada", diametro_barra, D)
+        if (D > 25 * Number(diametro_barra)){
+            switch (diametro_barra) {
+                case "10": return 190;
+                case "12.5": return 190;
+                case "16": return 190;
+                case "20": return 185;
+                case "25": return 175;
+                case "32": return 165;
+                default: return null;
+            }
+            
+        }
+        else if (Number(diametro_barra) >= 20 && D > (8 * Number(diametro_barra))) {
+            
+            switch (diametro_barra) {
+                case "10": return 105;
+                case "12.5": return 105;
+                case "16": return 105;
+                case "20": return 105;
+                case "25": return 95;
+                case "32": return 90;
+                default: return null;
+            }
+        } else if (Number(diametro_barra) < 20 && D >= 5 * Number(diametro_barra)) {
+            switch (diametro_barra) {
+                case "10": return 90;
+                case "12.5": return 90;
+                case "16": return 90;
+                default: return null;
+
+            }
+        } else if (Number(diametro_barra) < 10 && D > 3 * Number(diametro_barra)) {
+            switch (diametro_barra) {
+                case "10": return 85;
+
+                default: return null;
+
+            }
+        }
+    } 
+    return null
+}
--- software_engenharia/src/app/api/(estruturas_concreto)/viga_concreto/route.ts
+++ software_engenharia/src/app/api/(estruturas_concreto)/viga_concreto/route.ts
@@ -1,8 +1,7 @@
 
 import { NextRequest, NextResponse } from "next/server"
 import z from "zod"
-import { classe_concreto, diametros_comerciais } from "../coeficientes"
-import { fck } from "@/funcoes_coeficientes/viga_mista_alma_cheia/resultados_parciais"
+import { calc_props_secao_ii, calc_sigma_c, calc_sigma_s, classe_aco, classe_concreto, diametros_comerciais, props_conc, props_steel, sel_as, sel_delta_fsd } from "../coeficientes"
 const props_secao = z.object({
     bw: z.coerce.number(),
     h: z.coerce.number(),
@@ -30,9 +29,10 @@ const combinacoes_esforcos = z.object({
 })
 
 const props_aco = z.object({
-    fyk: z.coerce.number(),
+    classe_aco_long: z.enum(classe_aco),
     gama_a1: z.coerce.number(),
     fi_asw: z.coerce.number(),
+    classe_aco_transv: z.enum(classe_aco),
     arranjo_arm_pos: z.array(arranjo_aco).min(1),
     arranjo_arm_neg: z.array(arranjo_aco).min(1)
 })
@@ -52,23 +52,111 @@ export async function POST(req: NextRequest) {
     const body: ISchemaEntrada = await req.json();
 
     const dados = SchemaEntrada.safeParse(body);
-    
+
     if (dados.success) {
-        const fck_conc = fck(body.props_concreto.classe_concreto)
-        let as = 0
-        const d = body.props_secao.h-(body.props_secao.cobrimento+body.props_aco.fi_asw/10 + 0.5)
-        const fc = 0.85*fck_conc/10
+        const gama_c = body.props_concreto.gama_c
+        const results: Record<string, any> = {}
+        const { fyk, Es } = props_steel(body.props_aco.classe_aco_long)
+        const { fyk: fykw, Es: Esw } = props_steel(body.props_aco.classe_aco_long)
+        const { fck, Ec } = props_conc(body.props_concreto.classe_concreto)
+        const d = body.props_secao.h - (body.props_secao.cobrimento + body.props_aco.fi_asw / 10 + 0.5)
+        const d_linha = body.props_secao.h - d
+        const fc = 0.85 * fck / 10
         const bw = body.props_secao.bw
-        const fyd = (body.props_aco.fyk/10)/body.props_aco.gama_a1
+        const fyd = (fyk / 10) / body.props_aco.gama_a1
+        const alfa_e = Es / Ec
+        const as = body.props_aco.arranjo_arm_pos.
+            reduce((as_ant, as_cur) => (as_ant + sel_as[as_cur.diametro].as * as_cur.quantidade_barras), 0)
+        const as_linha = body.props_aco.arranjo_arm_neg.
+            reduce((as_ant, as_cur) => (as_ant + sel_as[as_cur.diametro].as * as_cur.quantidade_barras), 0)
+        const { xii, Iii } = calc_props_secao_ii({ alfa_e, as, as_linha, bw, d, d_linha })
+        const combs_fadiga = body.combinacoes_esforcos.filter(comb => comb.tipo === "FADIGA")
+
+        //verificação à fadiga da armação
+        if (combs_fadiga.length > 0) {
+            const momentos_positivos = combs_fadiga.filter(e => e.msd > 0)
+            const momentos_negativos = combs_fadiga.filter(e => e.msd < 0)
+            const resultados_fadiga_aco_pos = momentos_positivos.map(combinacao_fadiga => {
+                const msd = Math.abs(combinacao_fadiga.msd)
+                return calc_sigma_s({ alfa_e, d, Iii, msd, xii })
+            })
+
+            const resultados_fadiga_aco_neg = momentos_negativos.map(combinacao_fadiga => {
+                const msd = Math.abs(combinacao_fadiga.msd)
+                return calc_sigma_s({ alfa_e, d, Iii, msd, xii })
+            })
+            const maior_momento_pos = Math.max(...momentos_positivos.map(m => Math.abs(m.msd)))
+            const menor_momento_pos = Math.min(...momentos_positivos.map(m => Math.abs(m.msd)))
+            const maior_momento_neg = Math.max(...momentos_negativos.map(m => Math.abs(m.msd)))
+            const menor_momento_neg = Math.min(...momentos_negativos.map(m => Math.abs(m.msd)))
+            const fcd_fad = 0.45 * fck / (gama_c * 10) //kN/cm²
+
+            if (maior_momento_neg - menor_momento_neg > 0) {
+                const sigma_c_min = calc_sigma_c({msd: menor_momento_neg, Iii, xii})
+                const sigma_c_max = calc_sigma_c({msd: maior_momento_neg, Iii, xii})
+                const ni_c = Math.max(sigma_c_min.ni_c, sigma_c_max.ni_c)
+                
+                const solicitacao = ni_c * sigma_c_max.sigma_c_max
+                results["verficacao_fadiga_concreto"] = {
+                    ...results["verficacao_fadiga_concreto"],
+                    ni_c_neg:ni_c, sigma_c_min_neg:sigma_c_min.sigma_c_min, sigma_c_max_neg:sigma_c_max.sigma_c_max, tensao_fadiga_solicitante_neg: solicitacao, fcd_fad_neg: fcd_fad,    
+                    verificacao_neg: solicitacao <= fcd_fad ? "OK" : "Falha"
+                }
+                //gama_f nao adicionado por que é combinação frequente
+            }
+            if (maior_momento_pos - menor_momento_pos > 0) {
+                const sigma_c_min = calc_sigma_c({msd: menor_momento_pos, Iii, xii})
+                const sigma_c_max = calc_sigma_c({msd: maior_momento_pos, Iii, xii})
+                const ni_c = Math.max(sigma_c_min.ni_c, sigma_c_max.ni_c)
+                
+                const solicitacao = ni_c * sigma_c_max.sigma_c_max
+                results["verficacao_fadiga_concreto"] = {
+                    ...results["verficacao_fadiga_concreto"],
+                    ni_c_pos:ni_c, sigma_c_min_pos:sigma_c_min.sigma_c_min, sigma_c_max_pos:sigma_c_max.sigma_c_max, tensao_fadiga_solicitante_pos: solicitacao, fcd_fad_pos: fcd_fad,    
+                    verificacao_pos: solicitacao <= fcd_fad ? "OK" : "Falha"
+                }
+                //gama_f nao adicionado por que é combinação frequente
+
+            }
+
+            const delta_sigma_pos = Math.abs(Math.max(...resultados_fadiga_aco_pos) - Math.min(...resultados_fadiga_aco_pos))
+            const delta_sigma_neg = Math.abs(Math.max(...resultados_fadiga_aco_neg) - Math.min(...resultados_fadiga_aco_neg))
+            const var_tensao_arm_pos = Math.min(...body.props_aco.arranjo_arm_pos.map(arranjo => {
+                console.log(arranjo)
+                console.log("Teste", sel_delta_fsd(arranjo.diametro, body.props_aco.classe_aco_long))
+                return sel_delta_fsd(arranjo.diametro, body.props_aco.classe_aco_long) ?? 0
+            }))
+            const var_tensao_arm_neg = Math.min(...body.props_aco.arranjo_arm_neg.map(arranjo => {
+                return sel_delta_fsd(arranjo.diametro, body.props_aco.classe_aco_long) ?? 0
+            }))
+
+            results["verficacao_fadiga_aco"] = {
+                delta_sigma_pos,
+                var_tensao_arm_pos,
+                resultado_arm_pos: var_tensao_arm_pos > delta_sigma_pos ? "Ok" : "Falha",
+                delta_sigma_neg, var_tensao_arm_neg,
+                resultado_arm_neg: var_tensao_arm_neg > delta_sigma_neg ? "Ok" : "Falha",
+
+            }
+        }
+
+
         const resultados_as = body.combinacoes_esforcos.filter(e => e.tipo === "ELU").map(combinacao_esforcos => {
-            
-            const k = combinacao_esforcos.msd*100/(fc*bw*d**2)
-            const alfa = (1-Math.sqrt(1-2*k))
-            const y = alfa*d
-            const as_cur = fc*bw/fyd*y
-            return {k, as_cur, y, d, fyd, bw, fc}
+            const msd = Math.abs(combinacao_esforcos.msd * 100)
+            const k = msd / (fc * bw * d ** 2)
+            const alfa = (1 - Math.sqrt(1 - 2 * k))
+            const y = alfa * d
+            let as_nec = 0
+            let as_linha_nec = 0
+            if (combinacao_esforcos.msd > 0) {
+                as_nec = fc * bw / fyd * y
+            } else {
+                as_linha_nec = fc * bw / fyd * y
+            }
+
+            return { k, as_nec, as_linha_nec, y, d, fyd, bw, fc, xii, Iii }
         })
-        return NextResponse.json({ resultados_as });
+        return NextResponse.json({ results, resultados_as });
     } else {
         return NextResponse.json({ error: dados.error.issues.map(e => ({ message: e.message, path: e.path.join('.') })) }, { status: 400 });
     }
